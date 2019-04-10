a7eea2d5326a10d986a19e78a100ce50fea7aaf6
Rafael Ribeiro de Sousa <rafaelribeiro@softbox.com.br>
2019-04-10 10:59:48 -0300

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavbar from './components/layout/AppNavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <h1>Hello</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/layout/AppNavBar.jsx
+++ src/components/layout/AppNavBar.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class AppNavBar extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            ClientPanel
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarMain"
+          >
+            <span className="navbar-toggler-iccon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarMain">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
