4021f6c17e40b0e006ccf375c752101fe17040ce
Rafael Ribeiro de Sousa <rafaelribeiro@softbox.com.br>
2019-04-11 16:35:44 -0300

    fim aula 59

--- src/components/clients/Clients.jsx
+++ src/components/clients/Clients.jsx
@@ -4,10 +4,27 @@ import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
+  state = {
+    totalOwed: null
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    const { clients } = props;
+    if (clients) {
+      const total = clients.reduce((total, client) => {
+        return total + parseFloat(client.balance.toString());
+      }, 0);
+      return { totalOwed: total };
+    }
+    return null;
+  }
+
   render() {
     const { clients } = this.props;
+    const { totalOwed } = this.state;
     if (clients) {
       return (
         <div>
@@ -18,6 +35,12 @@ class Clients extends Component {
               </h2>
             </div>
             <div className="col-md-6">
+              <h5 className="text-right text-secondary">
+                Total Owed{' '}
+                <span className="text-primary">
+                  ${parseFloat(totalOwed).toFixed(2)}
+                </span>
+              </h5>
             </div>
             <table className="table table-striped">
               <thead className="thead-inverse">
@@ -47,7 +70,7 @@ class Clients extends Component {
         </div>
       );
     } else {
-      return <h1>Loading ...</h1>
+      return <Spinner/>
     }
   }
 }
