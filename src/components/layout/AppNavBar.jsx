fae056d20a1314d4e55957eef7d1f13fe5d929d2
Rafael Ribeiro de Sousa <rafaelribeiro@softbox.com.br>
2019-04-16 18:58:33 -0300

    metade da aula

--- src/components/layout/AppNavBar.jsx
+++ src/components/layout/AppNavBar.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
 
 class AppNavBar extends Component {
+  state = {
+    isAuthenticated: false
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    const { auth } = props;
+
+    if (auth.uid) {
+      return { isAuthenticated: true }
+    } else {
+      return { isAuthenticated: false }
+    }
+  }
+
   render() {
+    const { isAuthenticated } = this.state;
+    const { auth } = this.props;
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
         <div className="container">
@@ -19,17 +39,36 @@ class AppNavBar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarMain">
             <ul className="navbar-nav mr-auto">
+            {isAuthenticated ? (
               <li className="nav-item">
                 <Link to="/" className="nav-link">
                   Dashboard
                 </Link>
               </li>
+            ) : null}
             </ul>
+            {isAuthenticated ? (
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a href="#!" className="nav-link" onClick={this.onLogOutClick}>
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            ) : null}
           </div>
         </div>
       </nav>
     )
   }
 }
-
-export default AppNavBar;
+AppNavBar.propTypes = {
+  firebase: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+export default compose(
+  firebaseConnect(),
+  connect((state, props) => ({
+    auth: state.firebase.auth
+  }))
+) ;
